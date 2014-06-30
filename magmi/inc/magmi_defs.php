32e15241a31e5ca2dbc8366a1d5acf8c67d1eac2
dweeves <dweeves@gmail.com>
2014-07-01 00:56:15 +0200

    force timezone to UTC (for product update) update version to 0.7.20_git

--- magmi/inc/magmi_defs.php
+++ magmi/inc/magmi_defs.php
@@ -6,9 +6,6 @@ define("MAGMI_ENGINE_DIR", MAGMI_BASEDIR . "/engines");
 set_include_path(
     ini_get("include_path") . PATH_SEPARATOR . MAGMI_INCDIR . PATH_SEPARATOR . MAGMI_INTEGRATION_INCDIR . PATH_SEPARATOR .
          MAGMI_ENGINE_DIR);
-$dtz = date_default_timezone_get();
-if ($dtz == "")
-{
-    date_default_timezone_set("UTC");
-}
+//force UTC date
+         date_default_timezone_set("UTC");
 require_once ('magmi_loggers.php');
--- magmi/inc/magmi_version.php
+++ magmi/inc/magmi_version.php
@@ -1,5 +1,5 @@
 <?php
     	class Magmi_Version
     	{
-    		 public static $version="0.7.19a";
+    		 public static $version="0.7.20_git";
     	}
