518879cccaca3f1955435cd7fec84532df03d72a
dweeves <dweeves@gmail.com>
2014-09-26 00:03:08 +0200

    safer category removal

--- magmi/plugins/utilities/clearproductsandcategories/clearproductsandcategories.php
+++ magmi/plugins/utilities/clearproductsandcategories/clearproductsandcategories.php
@@ -29,10 +29,7 @@ class ClearProductandcategoryUtility extends Magmi_UtilityPlugin
             "catalog_product_website","catalog_product_entity","catalog_product_relation","cataloginventory_stock",
             "cataloginventory_stock_item","cataloginventory_stock_status",
 
-            "catalog_category_product_index","catalog_category_product","catalog_category_entity",
-            "catalog_category_entity_datetime","catalog_category_entity_decimal","catalog_category_entity_int",
-            "catalog_category_entity_text","catalog_category_entity_varchar",
-
+            "catalog_category_product_index","catalog_category_product",
             "catalogrule_affected_product","catalogrule_product","catalogrule_product_price","catalogsearch_fulltext",
             "catalogsearch_query","catalogsearch_result","product_alert_price","product_alert_stock","tag",
             "tag_properties","tag_relation","tag_summary",
@@ -71,67 +68,11 @@ class ClearProductandcategoryUtility extends Magmi_UtilityPlugin
         $sql = "SET FOREIGN_KEY_CHECKS = 1";
         $this->exec_stmt($sql);
         
-        $tname = $this->tablename("eav_entity_type");
-        $sql = "select entity_type_id from $tname where entity_model='catalog/category'";
-        $entity_type_id = $this->selectone($sql, null, "entity_type_id");
+        //safely remove all non root categories (not destroying structural categories)
+        //all sub values would be removed by cascading triggers.
+        $sql="DELETE FROM catalog_category_entity WHERE level>1";
+		 $this->exec_stmt($sql);
         
-        $tname = $this->tablename("eav_attribute_set");
-        $sql = "select attribute_set_id from $tname where entity_type_id=$entity_type_id";
-        $attributeSetId = $this->selectone($sql, null, "attribute_set_id");
-        
-        $tname = $this->tablename("eav_attribute");
-        
-        $sql = "select attribute_id from $tname where entity_type_id={$entity_type_id} and attribute_code='is_active'";
-        $isactive_id = $this->selectone($sql, null, "attribute_id");
-        
-        $sql = "select attribute_id from $tname where entity_type_id={$entity_type_id} and attribute_code='include_in_menu'";
-        $includeinmenu_id = $this->selectone($sql, null, "attribute_id");
-        
-        $sql = "select attribute_id from $tname where entity_type_id={$entity_type_id} and attribute_code='url_key'";
-        $urlkey_id = $this->selectone($sql, null, "attribute_id");
-        
-        $sql = "select attribute_id from $tname where entity_type_id={$entity_type_id} and attribute_code='name'";
-        $name_id = $this->selectone($sql, null, "attribute_id");
-        
-        $sql = "select attribute_id from $tname where entity_type_id={$entity_type_id} and attribute_code='display_mode'";
-        $displaymode_id = $this->selectone($sql, null, "attribute_id");
-        
-
-        //Base categories in the tree
-        $sql = "insert  into " . $this->tablename("catalog_category_entity") . " ";
-        $sql .= "(entity_id,entity_type_id,attribute_set_id,parent_id,created_at,updated_at,path,position,level,children_count) values ";
-        $sql .= "(1,{$entity_type_id},{$attributeSetId},0,'0000-00-00 00:00:00','".strftime('%Y-%m-%d %H:%M:%S')."','1'  ,1,0,1),";
-        $sql .= "(2,{$entity_type_id},{$attributeSetId},1,'0000-00-00 00:00:00','".strftime('%Y-%m-%d %H:%M:%S')."','1/2'  ,2,1,1),";
-        $this->exec_stmt($sql);
-       
-        //Default settings (active,include in menu)
-        $sql = "insert  into " . $this->tablename("catalog_category_entity_int") . " ";
-        $sql .= "(value_id,entity_type_id,attribute_id,store_id,entity_id,value) values ";
-        $sql .= "(1,{$entity_type_id},{$isactive_id},0,1,1),";
-        $sql .= "(2,{$entity_type_id},{$includeinmenu_id},0,1,1),";
-        $sql .= "(3,{$entity_type_id},{$isactive_id},0,2,1),";
-        $sql .= "(4,{$entity_type_id},{$includeinmenu_id},0,2,1),";
-        $sql .= "(5,{$entity_type_id},{$isactive_id},1,2,1),";
-        $sql .= "(6,{$entity_type_id},{$includeinmenu_id},1,2,1),";
-        
-        $this->exec_stmt($sql);
-        
-        //Fix inserting default values for categories & root cat in both admin & base store
-        $sql = "insert  into " . $this->tablename("catalog_category_entity_varchar") . " ";
-        $sql .= "(value_id,entity_type_id,attribute_id,store_id,entity_id,value) values ";
-        $sql .= "(1,{$entity_type_id},{$name_id},0,1,'Root Catalog'),";
-        $sql .= "(2,{$entity_type_id},{$name_id},1,1,'Root Catalog'),";      
-        $sql .= "(3,{$entity_type_id},{$displaymode_id},0,1,'PRODUCTS'),";
-        $sql .= "(4,{$entity_type_id},{$displaymode_id},1,1,'PRODUCTS'),";
-        $sql .= "(5,{$entity_type_id},{$urlkey_id},0,1,'root-catalog'),";
-        $sql .= "(6,{$entity_type_id},{$urlkey_id},1,1,'root-catalog'),";
-        $sql .= "(7,{$entity_type_id},{$name_id},0,2,'Default Category'),";
-        $sql .= "(8,{$entity_type_id},{$name_id},1,2,'Default Category'),";
-        $sql .= "(9,{$entity_type_id},{$displaymode_id},0,2,'PRODUCTS'),";
-        $sql .= "(10,{$entity_type_id},{$displaymode_id},1,2,'PRODUCTS'),";
-        $sql .= "(11,{$entity_type_id},{$urlkey_id},0,2,'default-category')";
-        $sql .= "(12,{$entity_type_id},{$urlkey_id},0,2,'default-category')";
-        $this->exec_stmt($sql);
         //default cat stock
         $sql = "insert  into " . $this->tablename("cataloginventory_stock") . " ";
         $sql .= "(stock_id,stock_name) values ";
