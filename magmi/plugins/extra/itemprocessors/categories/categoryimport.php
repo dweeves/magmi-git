3ef17770e0b9b01b373f9dbb9c92f6b30d795eed
dweeves <dweeves@gmail.com>
2014-11-18 01:33:45 +0100

    fixed multistore, multiroot management with "assign to all categories" setting.

--- magmi/plugins/extra/itemprocessors/categories/categoryimport.php
+++ magmi/plugins/extra/itemprocessors/categories/categoryimport.php
@@ -84,7 +84,7 @@ class CategoryImporter extends Magmi_ItemProcessor
 
     public function getPluginInfo()
     {
-        return array("name"=>"On the fly category creator/importer","author"=>"Dweeves","version"=>"0.2.4",
+        return array("name"=>"On the fly category creator/importer","author"=>"Dweeves","version"=>"0.2.5",
             "url"=>$this->pluginDocUrl("On_the_fly_category_creator/importer"));
     }
 
@@ -429,10 +429,19 @@ class CategoryImporter extends Magmi_ItemProcessor
             // assign to category roots
             if ($this->getParam("CAT:lastonly", 0) == 0)
             {
-                foreach (array_values($rootpaths) as $ra)
+                foreach ($rootpaths as $base=>$ra)
                 {
-                    $id = array_pop($ra["rootarr"]);
-                    $catids[] = $id;
+                    //find root lenght
+                    $bl=strlen($base);
+                    //for each part of category list to include upwards , match up to local root
+                    foreach($catlist as $catdef)
+                    {
+                        if(substr($catdef,0,$bl)==$base) {
+                            $rootpath=$ra['rootarr'];
+                            array_shift($rootpath);
+                            $catids= array_merge($catids,$rootpath);
+                        }
+                    }
                 }
             }
             $catids = array_unique($catids);
