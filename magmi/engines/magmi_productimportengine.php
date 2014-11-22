0933cd6e10b3a9adfbf9406032ed86f76627a785
Ted de Koning <teddk23@gmail.com>
2014-11-22 13:54:46 +0100

    Fixing bug causing empty categories to be imported

--- magmi/engines/magmi_productimportengine.php
+++ magmi/engines/magmi_productimportengine.php
@@ -1271,6 +1271,10 @@ class Magmi_ProductImportEngine extends Magmi_Engine
             // ow we have verified ids
             foreach ($cdata as $catid => $catpos)
             {
+            	if(empty($catid)) {
+            	    continue;
+            	}
+            	
                 $inserts[] = "(?,?,?)";
                 $data[] = $catid;
                 $data[] = $pid;
