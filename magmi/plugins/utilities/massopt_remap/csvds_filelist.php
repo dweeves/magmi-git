b9545dc4bd3b95cc3ddfe55eb795aebda42df5fd
dweeves <dweeves@gmail.com>
2015-04-15 14:46:28 +0200

    fix for csv file list

--- magmi/plugins/utilities/massopt_remap/csvds_filelist.php
+++ magmi/plugins/utilities/massopt_remap/csvds_filelist.php
@@ -1,21 +1,30 @@
 <?php
+if(!session_id()) {
+    session_start();
+}
 $files = $this->getCSVList();
+$token=$_SESSION["token"];
+$cfname=$this->getParam("CSV:filename");
+if($cfname==null)
+{
+    $cfname=$files[0];
+}
 if ($files !== false && count($files) > 0)
 {
     ?>
 <select name="CSV:filename" id="csvfile">
 	<?php foreach($files as $fname){ ?>	
-		<option <?php if($fname==$this->getParam("CSV:filename")){?>
-		selected=selected <?php }?> value="<?php echo $fname?>"><?php echo basename($fname)?></option>
+		<option <?php if($fname==$cfname){?>
+		selected=selected <?php }?> value="<?php echo $cfname?>"><?php echo basename($fname)?></option>
 	<?php }?>
 </select>
 <a id='csvdl'
-	href="./download_file.php?file=<?php echo $this->getParam("CSV:filename")?>">Download
+	href="./download_file.php?file=<?php echo $cfname?>&token=<?php echo $token?>">Download
 	CSV</a>
 <script type="text/javascript">
  $('csvdl').observe('click',function(el){
 	    var fval=$('csvfile').value;
- 		$('csvdl').href="./download_file.php?file="+fval;}
+ 		$('csvdl').href="./download_file.php?file="+fval+"&token=<?php echo $token?>";}
 	);
 </script><?php } else {?>
 <span> No csv files found in <?php echo $this->getScanDir(false)?></span>
