b713c306a1f287b377eb830d64879891f17dad98
Liam Wiltshire <liam@w.iltshi.re>
2015-10-11 22:23:17 +0100

    Authentication Using Admin Details

---
+++ magmi/inc/magmi_auth.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once("dbhelper.class.php");
+require_once("magmi_config.php");
+
+/**
+ * This class is the new authentication class for magmi
+ * Alot of the methods are based on code from Magento
+ *
+ * @author liam-wiltshire
+ *
+ */
+
+class Magmi_Auth extends DBHelper {
+    
+    private $user;
+    private $pass;
+    private $tablename;
+    protected $_conf;
+    
+    public function __construct($user,$pass){
+        parent::__construct();
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->_conf = Magmi_Config::getInstance();
+        $this->_conf->load();
+        
+        $host = $this->_conf->get("DATABASE","host","localhost");
+        $dbname = $this->_conf->get("DATABASE","dbname","magento");
+        $user = $this->_conf->get("DATABASE","user");
+        $pass = $this->_conf->get("DATABASE","password");
+        $port = $this->_conf->get("DATABASE","port", "3306");
+        
+        $this->initDb($host, $dbname, $user, $pass, $port, $socket, $conn, $debug);        
+        $this->tablename = $this->_conf->get("DATABASE", "table_prefix") . "admin_user";
+        
+    }
+    
+    public function authenticate(){
+        $result = $this->select("SELECT * FROM {$this->tablename} WHERE username = ?",array($this->user))->fetch(PDO::FETCH_ASSOC);
+        return $this->validatePass($result['password'],$this->pass);
+    }
+    
+    private function validatePass($hash,$pass){
+        $hash = explode(":",$hash);
+        
+        $check = md5($hash[1] . $pass);
+        
+        return $check == $hash[0];
+    }
+    
+}
